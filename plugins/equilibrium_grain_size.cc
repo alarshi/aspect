cba3a35d1d1a24b6e5655064417d8effc60a9209
Arushi Saxena <asaxena1@login02.expanse.sdsc.edu>
2021-06-18 06:40:29 -0700

    Changes in viscosity for smooth faults.

--- plugins/equilibrium_grain_size.cc
+++ plugins/equilibrium_grain_size.cc
@@ -323,8 +323,8 @@ namespace aspect
 	  if (use_faults)
             {
               unsigned int fault_index = this->introspection().compositional_index_for_name("faults");
-              if (in.composition[i][fault_index] > 0.05)
-                  out.viscosities[i] = 1e21 * in.composition[i][fault_index];
+              if (in.composition[i][fault_index] > 0 )
+                  out.viscosities[i] = pow (10, ( (20 - 24) * in.composition[i][fault_index] + 24 ));
             }
         }
       return;
